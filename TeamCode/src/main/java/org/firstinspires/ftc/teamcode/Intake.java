e2901086cc4e91d3de3359bda72f3ca341d63557
Jumpypants <jumpypants10@gmail.com>
2024-10-22 21:04:48 -0700

    Tuned pid for intake and outtake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -1,28 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
 public class Intake {
-    public static double ROTATIONS_PER_INCH = 1.0;
+    public static double SLIDE_ROTATIONS_PER_INCH = 4.4 * 3;
     public static double ALLOWED_ERROR = 0.1;
 
-    public static double KP = 0.05;
+    public static double KP = 0.07;
     public static double KI = 0.0;
     public static double KD = 0.0;
 
     private final Motor SLIDE_MOTOR;
+    private final SimpleServo WRIST_SERVO;
+    private final Motor SPIN_MOTOR;
 
     private double slidePosition = 0.0;
 
     public Intake(HardwareMap hardwareMap) {
-        this.SLIDE_MOTOR = new Motor(hardwareMap, "intakeSLide");
+        this.SLIDE_MOTOR = new Motor(hardwareMap, "intakeSlide");
+        this.WRIST_SERVO = new SimpleServo(hardwareMap, "intakeWrist", 0, Math.PI / 2);
+        this.SPIN_MOTOR = new Motor(hardwareMap, "intakeSpin");
+        SLIDE_MOTOR.setInverted(true);
     }
 
     public boolean stepSlideTo(double position) {
-        slidePosition = SLIDE_MOTOR.getCurrentPosition() * ROTATIONS_PER_INCH;
+        slidePosition = getSlidePosition();
 
         if (Math.abs(slidePosition - position) < ALLOWED_ERROR) {
             SLIDE_MOTOR.set(0);
@@ -40,7 +46,20 @@ public class Intake {
         return false;
     }
 
+    public void setWrist (double p) {
+        WRIST_SERVO.setPosition(p);
+    }
+
+    public void setSpin (double p) {
+        SPIN_MOTOR.set(p);
+    }
+
     public double getSlidePosition() {
+        slidePosition = (double) (SLIDE_MOTOR.getCurrentPosition()) / 360 * SLIDE_ROTATIONS_PER_INCH;
         return slidePosition;
+    }
+
+    public Motor getSLIDE_MOTOR() {
+        return SLIDE_MOTOR;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outtake.java
@@ -6,40 +6,47 @@ import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @Config
 public class Outtake {
     private final Motor SLIDE_MOTOR;
     private final ServoEx ROTATE_SERVO;
 
-    public static double SLIDE_ROTATIONS_PER_INCH = 1.0;
+    public static double SLIDE_ROTATIONS_PER_INCH = 4.724;
     public static double ALLOWED_ERROR = 0.1;
+
+    private double slidePosition = 0;
 
     public static double OUT_POSITION = 0.0;
     public static double IN_POSITION = Math.PI / 2;
 
-    public static double KP = 0.05;
+    public static double KP = 0.07;
     public static double KI = 0.0;
     public static double KD = 0.0;
-    public static double KF = 0.0;
+    public static double KF = 0.205;
 
     public Outtake(HardwareMap hardwareMap) {
         SLIDE_MOTOR = new Motor(hardwareMap, "outtakeSlide");
+        SLIDE_MOTOR.setInverted(true);
         ROTATE_SERVO = new SimpleServo(hardwareMap, "outtakeSpin", 0, Math.PI / 2);
     }
 
-    public boolean stepSlideTo(double position) {
-        double slidePosition = SLIDE_MOTOR.getCurrentPosition() * SLIDE_ROTATIONS_PER_INCH;
+    public boolean stepSlideTo(double position, Telemetry telemetry) {
+        slidePosition = getSlidePosition();
 
         if (Math.abs(slidePosition - position) < ALLOWED_ERROR) {
             SLIDE_MOTOR.set(0);
             return true;
         }
 
-        PIDFController pidfController = new PIDFController(KP, KI, KD, KF);
+        PIDController pidController = new PIDController(KP, KI, KD);
 
-        pidfController.setSetpoint(position);
+        pidController.setSetpoint(position);
 
-        double power = pidfController.calculate(slidePosition);
+        double power = pidController.calculate(slidePosition) + KF;
+        telemetry.addData("Slide Power", power);
+        telemetry.addData("KP", KP);
 
         SLIDE_MOTOR.set(power);
 
@@ -56,5 +63,14 @@ public class Outtake {
 
         ROTATE_SERVO.setPosition(IN_POSITION);
 
+    }
+
+    public Motor getSLIDE_MOTOR() {
+        return SLIDE_MOTOR;
+    }
+
+    public double getSlidePosition() {
+        slidePosition = SLIDE_MOTOR.getCurrentPosition() * SLIDE_ROTATIONS_PER_INCH / 360;
+        return slidePosition;
     }
 }
