6c26a0405c090a25bff9e2405b80a516a5f93e69
Jumpypants <jumpypants10@gmail.com>
2025-02-21 06:59:53 -0800

    Vision and sample selection with low-flip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Kicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Kicker.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.murphy.MurphyTask;
@@ -39,6 +44,31 @@ public class Kicker {
         @Override
         protected boolean run(Telemetry telemetry) {
             if (ELAPSED_TIME.seconds() > KICK_HALF_TIME) {
+                kicker.setKicker(IN_POSITION);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public static class KickActionRR implements Action {
+        private final ElapsedTime elapsedTime = new ElapsedTime();
+        private boolean initialized = false;
+        private final Kicker kicker;
+
+        public KickActionRR (Kicker kicker) {
+            this.kicker = kicker;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            if(!initialized) {
+                initialized = true;
+                elapsedTime.reset();
+                kicker.setKicker(OUT_POSITION);
+            }
+
+            if (elapsedTime.seconds() > KICK_HALF_TIME) {
                 kicker.setKicker(IN_POSITION);
                 return false;
             }
